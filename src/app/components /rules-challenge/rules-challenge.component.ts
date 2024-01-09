fe0076cf0669b2310c26bf2266c12030388505ed
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2024-01-09 23:02:52 +0100

    budget modified

--- src/app/components /rules-challenge/rules-challenge.component.ts
+++ src/app/components /rules-challenge/rules-challenge.component.ts
@@ -10,23 +10,13 @@ import { NgxExtendedPdfViewerService, pdfDefaultOptions } from 'ngx-extended-pdf
 })
 export class RulesChallengeComponent {
 
-  public page = 5;
+  public page = 1;
 
   public pageLabel!: string;
-  /** In most cases, you don't need the NgxExtendedPdfViewerService. It allows you
-   *  to use the "find" api, to extract text and images from a PDF file,
-   *  to print programmatically, and to show or hide layers by a method call.
-  */
+
   constructor(private pdfService: NgxExtendedPdfViewerService) {
-    /* More likely than not you don't need to tweak the pdfDefaultOptions.
-       They are a collecton of less frequently used options.
-       To illustrate how they're used, here are two example settings: */
+
     pdfDefaultOptions.doubleTapZoomFactor = '70%'; // The default value is '200%'
     pdfDefaultOptions.maxCanvasPixels = 4096 * 4096 * 5; // The default value is 4096 * 4096 pixels,
-    // but most devices support much higher resolutions.
-    // Increasing this setting allows your users to use higher zoom factors,
-    // trading image quality for performance.
-
-    
     }
 }
