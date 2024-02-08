e55b9636c6c0d5a1ce2d1f32a4809981c63ccdc9
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2024-02-08 16:13:28 +0100

    https on enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    _API_URL: "http://backend-challenge.galaxim.com",
-    _API_AUTH_URL: "http://backend-challenge.galaxim.com/api/v1/auth",
-    _API_GAME_URL: "http://backend-challenge.galaxim.com/api/v1/games",
-    _API_OFFICE_URL: "http://backend-challenge.galaxim.com/api/v1/offices",
-    _API_USER_URL: "http://backend-challenge.galaxim.com/api/v1/users"
+    _API_URL: "https://backend-challenge.galaxim.com",
+    _API_AUTH_URL: "https://backend-challenge.galaxim.com/api/v1/auth",
+    _API_GAME_URL: "https://backend-challenge.galaxim.com/api/v1/games",
+    _API_OFFICE_URL: "https://backend-challenge.galaxim.com/api/v1/offices",
+    _API_USER_URL: "https://backend-challenge.galaxim.com/api/v1/users"
 };
