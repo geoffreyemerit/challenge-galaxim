fe68bbb618d30085a7773712a1e7ae3059ebc97b
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2024-02-08 02:42:45 +0100

    pdf modified

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    _API_URL: "http://localhost:8080",
+    _API_AUTH_URL: "http://localhost:8080/api/v1/auth",
+    _API_GAME_URL: "http://localhost:8080/api/v1/games",
+    _API_OFFICE_URL: "http://localhost:8080/api/v1/offices",
+    _API_USER_URL: "http://localhost:8080/api/v1/users"
+};
